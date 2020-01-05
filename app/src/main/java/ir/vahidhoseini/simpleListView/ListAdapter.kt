6ec0c73da9efe87fc836609b9d38bf5ccb5cdc5f
vahid <vahid09390292608@gmail.com>
2020-01-05 12:06:18 +0330

    list adapter is now ready to use

--- app/src/main/java/ir/vahidhoseini/simpleListView/ListAdapter.kt
+++ app/src/main/java/ir/vahidhoseini/simpleListView/ListAdapter.kt
@@ -1,7 +1,6 @@
 package ir.vahidhoseini.simpleListView
 
 
-import android.R
 import android.content.Context
 import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.view.LayoutInflater
@@ -19,20 +18,21 @@ import java.util.ArrayList
 /**
  * Created by Vahid on 4/26/2016.
  */
-class ListAdapter(names: ArrayList<StructList>, context: Context) : RecyclerView.Adapter<ListAdapter.PersonViewHolder>() {
+class ListAdapter(items: ArrayList<StructList>, context: Context) :
+    RecyclerView.Adapter<ListAdapter.ItemViewHolder>() {
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        return names.size;
+        return items.size;
     }
 
-    private var names = ArrayList<StructList>()
+    private var items = ArrayList<StructList>()
     private var ctx = context
 
 //    val itemCount: Int
-//        get() = names.size
+//        get() = items.size
 
-    class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //            btnAdapter = (Button) itemView.findViewById(R.id.btnAdapter);
         //            txtAdapter = (TextView) itemView.findViewById(R.id.txtAdapter);
         //            chkAdapter = (CheckBox) itemView.findViewById(R.id.chkAdapter);
@@ -42,18 +42,19 @@ class ListAdapter(names: ArrayList<StructList>, context: Context) : RecyclerView
     }
 
     init {
-        this.names = names
+        this.items = items
         ctx = context
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): PersonViewHolder {
-        val lytInflater = LayoutInflater.from(ctx)
-        val view =lytInflater.inflate(R.layout.lyt_adapter_list, viewGroup, false)
-        return PersonViewHolder(view)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ItemViewHolder {
+        val lytInflater:LayoutInflater = LayoutInflater.from(ctx)
+        lytInflater.inflate(R.layout.activity_main , viewGroup)
+        var view = lytInflater.inflate(R.layout.lyt_adapter_list, viewGroup, false) as View
+        return ItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(personViewHolder: PersonViewHolder, i: Int) {
-        //        StructList name = names.get(i);
+    override fun onBindViewHolder(personViewHolder: ItemViewHolder, i: Int) {
+        //        StructList name = items.get(i);
         //        personViewHolder.txtAdapter.setText(name.firstName);
         //        personViewHolder.chkAdapter.setText(name.lastName);
         //        personViewHolder.btnAdapter.setText(""+name.age);
