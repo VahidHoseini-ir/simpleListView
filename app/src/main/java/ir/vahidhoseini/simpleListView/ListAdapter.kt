428adca823b122535fc1912fad639cd8cb69c450
vahid <vahid09390292608@gmail.com>
2020-01-05 13:06:55 +0330

    from here  i wan to make it independent list view
    i want from this project to not making new adapter for every new list just use this adapter and make it independent.

--- app/src/main/java/ir/vahidhoseini/simpleListView/ListAdapter.kt
+++ app/src/main/java/ir/vahidhoseini/simpleListView/ListAdapter.kt
@@ -3,6 +3,7 @@ package ir.vahidhoseini.simpleListView
 
 import android.content.Context
 import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +13,13 @@ import android.widget.TextView
 import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import java.security.Provider
-
-import java.util.ArrayList
+import java.util.*
 
 /**
  * Created by Vahid on 4/26/2016.
  */
-class ListAdapter(items: ArrayList<StructList>, context: Context) :RecyclerView.Adapter<ListAdapter.ItemViewHolder>() {
+class ListAdapter(items: ArrayList<StructList>, context: Context) :
+    RecyclerView.Adapter<ListAdapter.ItemViewHolder>() {
 
     override fun getItemCount(): Int {
         return items.size;
@@ -29,9 +30,9 @@ class ListAdapter(items: ArrayList<StructList>, context: Context) :RecyclerView.
 
 
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-         var btnAdapter: Button? = null
-         var txtAdapter: TextView = itemView.findViewById(R.id.item_title)
-         var chkAdapter: CheckBox? = null
+        var btnAdapter: Button? = null
+        var txtAdapter: TextView = itemView.findViewById(R.id.item_title)
+        var chkAdapter: CheckBox? = null
     }
 
     init {
@@ -40,17 +41,15 @@ class ListAdapter(items: ArrayList<StructList>, context: Context) :RecyclerView.
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ItemViewHolder {
-        val lytInflater:LayoutInflater = LayoutInflater.from(ctx)
+        val lytInflater: LayoutInflater = LayoutInflater.from(ctx)
         var view = lytInflater.inflate(R.layout.lyt_adapter_list, viewGroup, false) as View
         return ItemViewHolder(view)
     }
 
     override fun onBindViewHolder(personViewHolder: ItemViewHolder, i: Int) {
-                val item:StructList = items.get(i)
-                personViewHolder.txtAdapter.setText(item.title);
-
-        //        personViewHolder.chkAdapter.setText(name.lastName);
-        //        personViewHolder.btnAdapter.setText(""+name.age);
+        val item: StructList = items.get(i)
+        personViewHolder.txtAdapter.setText(item.title);
+        personViewHolder.txtAdapter.setBackgroundColor(Color.argb(255,Random().nextInt(256), Random().nextInt(256), Random().nextInt(256)))
     }
 
 
